5fa793e37a0d013928543ee5a786c76be95517a9
NethminiAdithya <nethminibandara225@gmail.com>
2026-06-05 10:13:18 -0700

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,8 @@
+const colors=[
+"Red",
+"Yellow",
+"Green"];
+
+for(let key of  colors){
+	console.log(key);
+}
---
+++ 5.js
@@ -0,0 +1,6 @@
+const multiply=(x,y)=>x*y;
+console.log(multiply(3,2));
+
+
+const sayHi=()=>console.log("Hi");
+ sayHi();
